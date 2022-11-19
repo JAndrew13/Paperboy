5a6b1da06a565ef59d8dd3daf5005b95ec0dd35c
Jake Brunner <Jbbrunner10@gmail.com>
2022-11-18 20:38:06 -0800

    reformated for html JS

--- app.js
+++ app.js
@@ -1,97 +1,126 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require("ejs");
+
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 const port = 3000;
+
 
 // =========== Paperboy Configuration ============ //
 
-const wallet = {
-  cash: 1000,  // set wallet balance
-  tokens: 0   // token balance
-};
-const rate = {
-  buy: 1,    // percent of cash to spend on action
-  sell: 1    // percent of tokens to sell on action
-};
 
-const accountValue = "-";
-const lastPrice = 0;
-var pageReport = {
-  Cash: wallet.cash,
-  Tokens: wallet.tokens,
-  AccoutValue: accountValue
+let algo = {
+  algoName: "Paperboy Algo 1",
+  buyRate: 1,
+  connection: undefined,
+  duration: 0,
+  gainLoss: 0,
+  lastPrice: 0,
+  nextMove: undefined,
+  profitLoss: 0,
+  sellRate: 1,
+  trades: 0
 }
+
+let chart = {
+  connectionStatus: undefined,
+  currPrice: undefined,
+  ticker: 'none',
+  timeFrame: ['1m', '3m', '5m', '10m', '15m', '30m', '1h', '2h', '4h', '1d'],
+}
+
+let wallet = {
+  accountValue: 0,
+  cash: 1000,
+  tokens: 0,
+}
+
 
 // ============== Server Functions =============== //
 
 app.use(express.json()); // allow app to receive JSON Data
 
-app.get("/test", function(req, res){
 
-  res.send(pageReport);
+// define home route
+// sends data to hompage for display to user
+app.get("/", function(req, res){
+  res.render("home", {
+    algo: algo,
+    chart: chart,
+    wallet: wallet,
+  });
 });
 
+
+// route for receiving trading view alerts
+// unpacks json data and sends to paperboy()
 app.post('/alert', (req, res) => {
   console.log("");
   console.log("Recived new" + req.body.action + "alert from Trading View!");
   console.log(req.body);  // log Raw alert
   // { ticker: 'MATICUSDT', action: 'Sell', price: '0.9634' }
 
-  const alert = { // Compile Alert Object
+  let alert = { // Compile Alert Object
     ticker: req.body.ticker, // 'MATICUSDT'
     action: req.body.action, // 'Sell' or 'Buy'
     price: req.body.price    // '0.9235'
   }
 
-  paperBoy(alert, wallet, rate); // Alert PaperBoy
+  
+  paperBoy(alert); // Alert PaperBoy
+
   return res.status(200).json({ ok: true });   // Return OK to server
 });
 
-// ============= PaperBoy functions ============== //
+// ============= Buy / Sell Actions ============== //
 
-function paperBoy(alert, wallet, rate){
-  console.log("");
-  console.log("~= PaperBoy =~");
+function paperBoy(alert){
 
   if (alert.action === "Buy"){
     //create new BUY order
       // determine amount to buy & prepare order
-      const orderAmount = (wallet.cash * rate.buy);
+      const orderAmount = (wallet.cash * algo.buyRate);
       const tokenAmount = (orderAmount / alert.price);
-      //update wallet
-      wallet.cash -= orderAmount;
-      wallet.tokens += tokenAmount;
+      // place order
+      buy(orderAmount, tokenAmount);
 
   } else if (alert.action === "Sell"){
     //create new SELL order
       // determine amount to buy & prepare order
-      const orderAmount = (wallet.tokens * rate.sell);
-      const cashAmount = (orderAmount * alert.price);
-      //update wallet
-      wallet.cash += cashAmount;
-      wallet.tokens -= orderAmount;
+      let orderAmount = (wallet.tokens * algo.sellRate);
+      let cashAmount = (orderAmount * alert.price);
+      // place order
+      sell(orderAmount, cashAmount)
   }
-  console.log("Order Complete!");
-  const lastPrice = alert.price;
-  report(wallet, alert.price);
-  console.log("~= PaperBoy =~");
-  console.log("");
-};
 
-function report(wallet, lastPrice){
-  console.log("~= Wallet Report =~")
-  console.log("")
-  console.log("Cash (USD): $" + wallet.cash);
-  console.log("Tokens: " + wallet.tokens);
-  console.log("");
-  console.log("Account Value:")
-  console.log("$"+ (wallet.cash + (wallet.tokens * lastPrice)));
-  accoutnValue = console.log("$"+ (wallet.cash + (wallet.tokens * lastPrice)));
-  return accountValue
+  updateReports(alert);
 
 };
 
+function buy(orderAmount, tokenAmount){
+  //update wallet
+  wallet.cash -= orderAmount;
+  wallet.tokens += tokenAmount;
+};
+
+function sell(orderAmount, cashAmount){
+  //update wallet
+  wallet.cash += cashAmount;
+  wallet.tokens -= orderAmount;
+};
+
+function updateReports(alert){
+  algo.lastPrice = alert.Price;
+  algo.trades += 1;
+  chart.ticker = alert.ticker;
+  chart.timeFrame = undefined;
+  wallet.accountValue = "$" + (wallet.cash + (wallet.tokens * alert.price)).toFixed(2);
+};
 // ============================================ //
 // Server startup logs
 app.listen(port, () => {console.log('🚀 Server running on port '+ port);
